24a3a745ab3d739facca7c454d016d339897562b
Cristian Cominelli <firegarden1977@gmail.com>
2020-03-09 13:59:21 +0100

    update controls

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var vid, seekslider;
 var video = document.querySelector('.video');
 var juice = document.querySelector('.orange-juice');
 var bar = document.querySelector('.orange-bar');
@@ -34,3 +35,46 @@ video.addEventListener('timeupdate', function() {
 
 
 bar.addEventListener('click', scrub);
+
+function initializePlayer() {
+    vid = document.querySelector(".video");
+    vid.addEventListener("timeupdate", seektimeupdate, false);
+    seekslider = document.querySelector(".orange-bar");
+    seekslider.addEventListener("change", vidSeek, false);
+    volumeslider.addEventListener("change", setvolume, false);
+    volumeslider = document.getElementById("volumeslider");
+}
+
+function vidSeek() {
+    var seekto = vid.duration * (seekslider.value / 100);
+    vid.currentTime = seekto;
+}
+
+function seektimeupdate() {
+    var nt = vid.currentTime * (100 / vid.duration);
+    seekslider.value = nt;
+    var curmins = Math.floor(vid.currentTime / 60);
+    var cursecs = Math.floor(vid.currentTime - curmins * 60);
+    var durmins = Math.floor(vid.duration / 60);
+    var dursecs = Math.floor(vid.duration - durmins * 60);
+    if (cursecs < 10) {
+        cursecs = "0" + cursecs;
+    }
+    if (dursecs < 10) {
+        dursecs = "0" + dursecs;
+    }
+    if (curmins < 10) {
+        curmins = "0" + curmins;
+    }
+    if (durmins < 10) {
+        durmins = "0" + durmins;
+    }
+
+    curtimetext.innerHTML = curmins + ":" + cursecs;
+    durtimetext.innerHTML = durmins + ":" + dursecs;
+}
+
+function setvolume() {
+    vid.volume = volumeslider.value / 100;
+}
+window.onload = initializePlayer;
